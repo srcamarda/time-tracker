04a9ad178bbfa57304b2069ee6ead128518b02a5
Paulo Macêdo <p.ricardomacedo@hotmail.com>
2024-04-04 10:43:28 -0300

    Criar classe Tarefa utilizando Design Pattern Builder

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 import pessoa.Pessoa;
 import pessoa.TipoCargo;
+import projeto.Tarefa;
 
 import java.util.UUID;
 
 public class Main {
     public static void main(String[] args) {
+        // Pessoa
         Pessoa pessoa = new Pessoa.Builder().nome("Paulo").cargo(TipoCargo.JUNIOR).build();
         System.out.println(pessoa);
 
@@ -12,5 +14,14 @@ public class Main {
         System.out.println(uuid);
         Pessoa pessoa2 = new Pessoa.Builder().id(uuid).nome("Pedro").cargo(TipoCargo.JUNIOR).build();
         System.out.println(pessoa2);
+
+        // Tarefa
+        Tarefa tarefa = new Tarefa.Builder().titulo("Estruturar projeto").build();
+        System.out.println(tarefa);
+
+        String uuidTarefa = UUID.randomUUID().toString();
+        System.out.println(uuidTarefa);
+        Tarefa tarefa1 = new Tarefa.Builder().id(uuidTarefa).titulo("Testar classes").pessoa(pessoa).build();
+        System.out.println(tarefa1);
     }
 }

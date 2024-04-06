9a4d27defb57c482e2a2f31a7b39ade8e706422b
Paulo Macêdo <44329969+prmacedo@users.noreply.github.com>
2024-04-06 20:32:29 -0300

    Adicionar método para buscar Pessoa por username

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import mockup.Mockup;
 import model.Pessoa;
 import model.Tarefa;
 import repository.*;
+import service.PessoaService;
 import utility.TipoCargo;
 import utility.TipoPlano;
 import utility.singleton.PessoaSingleton;
@@ -10,16 +11,6 @@ import utility.singleton.TarefaSingleton;
 
 public class Main {
     public static void main(String[] args) {
-        //Mockup.init();
-//        RepositoryPessoa repositoryPessoa = new RepositoryPessoa(ArquivoPaths.PESSOAS);
-//        RepositoryTarefa repositoryTarefa = new RepositoryTarefa(ArquivoPaths.TAREFAS, repositoryPessoa);
-//        RepositoryProjeto repositoryProjeto = new RepositoryProjeto(ArquivoPaths.PROJETOS, repositoryPessoa, repositoryTarefa);
-
-//        SistemaRepository sistema = SistemaRepository.INSTANCE;
-
-        PessoaSingleton pessoaSingleton = PessoaSingleton.INSTANCE;
-        TarefaSingleton tarefaSingleton = TarefaSingleton.INSTANCE;
-        ProjetoSingleton projetoSingleton = ProjetoSingleton.INSTANCE;
-        System.out.println("Hello");
+        System.out.println(PessoaService.buscarPessoa("  JuliA   "));
     }
 }

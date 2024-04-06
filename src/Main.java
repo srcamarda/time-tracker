9e5c6f7b23a4d2352962bdffc3732bcef7988d93
Paulo Macêdo <44329969+prmacedo@users.noreply.github.com>
2024-04-06 20:34:23 -0300

    Remover imports não utilizados no arquivo Main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,4 @@
-import mockup.Mockup;
-import model.Pessoa;
-import model.Tarefa;
-import repository.*;
 import service.PessoaService;
-import utility.TipoCargo;
-import utility.TipoPlano;
-import utility.singleton.PessoaSingleton;
-import utility.singleton.ProjetoSingleton;
-import utility.singleton.TarefaSingleton;
 
 public class Main {
     public static void main(String[] args) {

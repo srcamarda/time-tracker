8067349254d91e300868219fd17b37b78e74dcd5
Davi Jr <153877813+engdvj@users.noreply.github.com>
2024-04-07 20:49:58 -0300

    Testar busca

--- src/Main.java
+++ src/Main.java
@@ -12,18 +12,18 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-//        System.out.println(PessoaService.buscarPessoa("  JuliA   "));
-//        System.out.println(ProjetoService.buscarProjeto("Ap"));
+ //       System.out.println(PessoaService.buscarPessoas("  igor   "));
+ //       System.out.println(ProjetoService.buscarProjeto(""));
 //        System.out.println(TarefaService.buscarTarefa("desen"));
 //        Pessoa pessoa = PessoaService.buscarPessoa("  igor   ");
 //        System.out.println(TarefaService.buscarTarefa(Conversores.converterParaDTO(pessoa)));
-
-        Mockup.init();
-        Tarefa tarefaTeste = Conversores.converterParaModel(Mockup.tarefaDTOList.get(0));
-        System.out.println(tarefaTeste);
+//
+//        Mockup.init();
+//        Tarefa tarefaTeste = Conversores.converterParaModel(Mockup.tarefaDTOList.get(0));
+//        System.out.println(tarefaTeste);
 //        TarefaSingleton.INSTANCE.getRepositoryTarefa().salvarTarefa(tarefaTeste);
-
-        System.out.println(TarefaService.criarTarefa(tarefaTeste));
+//
+//        System.out.println(TarefaService.criarTarefa(tarefaTeste));
 
     }
 }

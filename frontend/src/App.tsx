8b81207753f87e9b0c185850eac82553eb69f62e
Paulo Macêdo <p.ricardomacedo@hotmail.com>
2024-04-26 21:26:19 -0300

    config css variables and create footer component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { TbBrandGithubFilled } from 'react-icons/tb'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+    <div className="w-full h-screen relative">
+      <h1>Hello World!</h1>
+
+      <footer className="absolute inset-x-0 bottom-0 bg-davys-gray text-white font-bold text-xs p-2">
+        <a href="https://github.com/srcamarda/time-tracker" target="_blank" className='flex gap-2 justify-center items-center'>
+          <TbBrandGithubFilled />
+          
+          Github
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </footer>
+    </div>
   )
 }
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -18,39 +18,13 @@ module.exports = {
     },
     extend: {
       colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        primary: {
-          DEFAULT: "hsl(var(--primary))",
-          foreground: "hsl(var(--primary-foreground))",
-        },
-        secondary: {
-          DEFAULT: "hsl(var(--secondary))",
-          foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
-        },
-        popover: {
-          DEFAULT: "hsl(var(--popover))",
-          foreground: "hsl(var(--popover-foreground))",
-        },
-        card: {
-          DEFAULT: "hsl(var(--card))",
-          foreground: "hsl(var(--card-foreground))",
-        },
+        white: "var(--white)",
+        peach: "var(--peach)",
+        dum: "var(--dum)",
+        "dim-gray": "var(--dim-gray)",
+        "davys-gray": "var(--davys-gray)",
+        cerise: "var(--cerise)",
+        sunglow: "var(--sunglow)"
       },
       borderRadius: {
         lg: "var(--radius)",

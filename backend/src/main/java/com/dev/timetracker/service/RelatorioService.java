b8e19ae01d3c2886ff97317885a338942ffa284a
Pedro Antonio Souza <pedroantoniiodm@gmail.com>
2024-04-29 14:25:37 -0300

    create new method for calculate user working time on all projects

--- backend/src/main/java/com/dev/timetracker/service/RelatorioService.java
+++ backend/src/main/java/com/dev/timetracker/service/RelatorioService.java
@@ -1,11 +1,16 @@
 package com.dev.timetracker.service;
 
+import com.dev.timetracker.dto.report.DTOProjectTime;
 import com.dev.timetracker.dto.report.DTOTimeWork;
 import com.dev.timetracker.dto.report.DTOUserTime;
+import com.dev.timetracker.entity.EntityProject;
 import com.dev.timetracker.entity.EntityTask;
 import com.dev.timetracker.entity.EntityUser;
 import com.dev.timetracker.repository.RepositoryTask;
+import com.dev.timetracker.repository.RepositoryUser;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
@@ -20,6 +25,8 @@ public class RelatorioService {
 
     @Autowired
     private RepositoryTask repositoryTask;
+    @Autowired
+    private RepositoryUser repositoryUser;
 
     public List<DTOUserTime> calculateUserTimeRankingForProject(Long projectId) {
         List<EntityTask> tasks = repositoryTask.findAllByProjectIdAndActiveTrue(projectId);
@@ -35,6 +42,7 @@ public class RelatorioService {
                 .sorted(Comparator.comparing(DTOUserTime::totalHours).reversed())
                 .collect(Collectors.toList());
     }
+
     public List<DTOTimeWork> calculateWorkForUser(Long userId, LocalDate startDate, LocalDate endDate) {
         Timestamp startTimestamp = Timestamp.valueOf(startDate.atStartOfDay());
         Timestamp endTimestamp = Timestamp.valueOf(endDate.atTime(LocalTime.MAX));
@@ -52,4 +60,29 @@ public class RelatorioService {
         }
         return report;
     }
+
+    public List<DTOProjectTime> hoursWorkedByUserAllProjects(Long userId) {
+        EntityUser user = repositoryUser.getReferenceById(userId);
+        Page<EntityTask> tasks = repositoryTask.findAllByIdUserAndActiveTrue(user, Pageable.unpaged());
+        Map<EntityProject, Long> projectTimeMap = new HashMap<>();
+
+        for (EntityTask task : tasks) {
+            EntityProject project = task.getProject();
+            long taskHours = calculateTaskHours(task);
+            projectTimeMap.put(project, projectTimeMap.getOrDefault(project, 0L) + taskHours);
+        }
+
+        List<DTOProjectTime> result = new ArrayList<>();
+        for (Map.Entry<EntityProject, Long> entry : projectTimeMap.entrySet()) {
+            EntityProject project = entry.getKey();
+            Long hoursWorked = entry.getValue();
+            result.add(new DTOProjectTime(project.getTitle(), hoursWorked));
+        }
+        return result;
+    }
+
+    private long calculateTaskHours(EntityTask task) {
+        return TimeUnit.MILLISECONDS.toHours(task.getEndTime().getTime() - task.getStartTime().getTime());
+    }
+
 }

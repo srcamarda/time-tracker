5eec105c2367a28e6a3cb9077d9858727dbabb28
Davi Jr <153877813+engdvj@users.noreply.github.com>
2024-04-29 00:23:34 -0300

    create method to calculate work for user

--- backend/src/main/java/com/dev/timetracker/service/RelatorioService.java
+++ backend/src/main/java/com/dev/timetracker/service/RelatorioService.java
@@ -1,5 +1,6 @@
 package com.dev.timetracker.service;
 
+import com.dev.timetracker.dto.report.DTOTimeWork;
 import com.dev.timetracker.dto.report.DTOUserTime;
 import com.dev.timetracker.entity.EntityTask;
 import com.dev.timetracker.entity.EntityUser;
@@ -7,6 +8,9 @@ import com.dev.timetracker.repository.RepositoryTask;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
@@ -30,5 +34,22 @@ public class RelatorioService {
                 .map(entry -> new DTOUserTime(entry.getKey().getName(), entry.getValue()))
                 .sorted(Comparator.comparing(DTOUserTime::totalHours).reversed())
                 .collect(Collectors.toList());
+    }
+    public List<DTOTimeWork> calculateWorkForUser(Long userId, LocalDate startDate, LocalDate endDate) {
+        Timestamp startTimestamp = Timestamp.valueOf(startDate.atStartOfDay());
+        Timestamp endTimestamp = Timestamp.valueOf(endDate.atTime(LocalTime.MAX));
+
+        List<EntityTask> tasks = repositoryTask.findByUserIdAndActiveTrueAndStartTimeBetweenAndEndTimeBetween(
+                userId, startTimestamp, endTimestamp);
+
+        List<DTOTimeWork> report = new ArrayList<>();
+        for (EntityTask task : tasks) {
+            long duration = task.getEndTime().getTime() - task.getStartTime().getTime();
+            long hours = TimeUnit.MILLISECONDS.toHours(duration);
+
+            DTOTimeWork dto = new DTOTimeWork(task.getProject().getTitle(), task.getTitle(), hours);
+            report.add(dto);
+        }
+        return report;
     }
 }

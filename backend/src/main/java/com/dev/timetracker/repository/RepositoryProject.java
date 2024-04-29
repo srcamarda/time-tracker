34fb55670bf4e77717065bbc7e5c9e2e3b486360
Pedro Antonio Souza <pedroantoniiodm@gmail.com>
2024-04-29 18:29:52 -0300

    Add get project for UsersId

--- backend/src/main/java/com/dev/timetracker/repository/RepositoryProject.java
+++ backend/src/main/java/com/dev/timetracker/repository/RepositoryProject.java
@@ -5,7 +5,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RepositoryProject extends JpaRepository<EntityProject, Long> {
     Page<EntityProject> findAllByActiveTrue(Pageable pageable);
     EntityProject findByIdAndActiveTrue(Long id);
+    List<EntityProject> findByUsersId(Long userId);
 }

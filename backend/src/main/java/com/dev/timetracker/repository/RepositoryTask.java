226dd8580d5902da89c9625e2b0119c31a0626d4
Davi Jr <153877813+engdvj@users.noreply.github.com>
2024-04-28 23:00:52 -0300

    Add new methods to find

--- backend/src/main/java/com/dev/timetracker/repository/RepositoryTask.java
+++ backend/src/main/java/com/dev/timetracker/repository/RepositoryTask.java
@@ -1,13 +1,21 @@
 package com.dev.timetracker.repository;
 
+import com.dev.timetracker.dto.report.DTOUserTime;
 import com.dev.timetracker.entity.EntityTask;
 import com.dev.timetracker.entity.EntityUser;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface RepositoryTask extends JpaRepository<EntityTask, Long> {
     Page<EntityTask> findAllByActiveTrue(Pageable pageable);
     EntityTask findByIdAndActiveTrue(Long id);
     Page<EntityTask> findAllByIdUserAndActiveTrue(EntityUser idUser, Pageable pageable);
+    List<EntityTask> findAllByProjectIdAndActiveTrue(Long projectId);
+    List<EntityTask> findByProjectIdAndActiveTrue(Long projectId);
+
 }

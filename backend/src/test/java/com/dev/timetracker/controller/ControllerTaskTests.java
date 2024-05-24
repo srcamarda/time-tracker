ab706d4820ba23654b897b93fade7d69aa3d7a7e
Pedro Antonio Souza <pedroantoniiodm@gmail.com>
2024-05-23 23:10:14 -0300

    Update ControllerTaskTests

--- backend/src/test/java/com/dev/timetracker/controller/ControllerTaskTests.java
+++ backend/src/test/java/com/dev/timetracker/controller/ControllerTaskTests.java
@@ -1,8 +1,93 @@
 package com.dev.timetracker.controller;
 
+import com.dev.timetracker.dto.task.DTOCreateTask;
+import com.dev.timetracker.entity.EntityTask;
+import com.dev.timetracker.entity.EntityUser;
+import com.dev.timetracker.repository.RepositoryTask;
+import com.dev.timetracker.repository.RepositoryUser;
+import com.dev.timetracker.utility.category.Tag;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.http.MediaType.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 @ExtendWith(MockitoExtension.class)
 public class ControllerTaskTests {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private RepositoryUser repositoryUser;
+
+    @MockBean
+    private RepositoryTask repositoryTask;
+
+    @InjectMocks
+    private ControllerTask controllerTask;
+
+    static DTOCreateTask taskDTO;
+    static EntityTask task;
+
+
+    @BeforeAll
+    public static void initializeTestTask() {
+
+        ControllerUserTests.initializeTestUser();
+
+        taskDTO = new DTOCreateTask(
+                1L,
+                "O Teste do Teste",
+                "vamos testar em tudo",
+                Tag.IN_PROGRESS,
+                ControllerUserTests.user,
+                Timestamp.valueOf(LocalDateTime.now()),
+                null
+        );
+
+        task = new EntityTask(taskDTO);
+        task.setId(1L);
+    }
+
+    @Test
+    public void shouldCreateNewTask() throws Exception {
+
+        Mockito.when(repositoryTask.findByIdAndActiveTrue(task.getId())).thenReturn(task);
+
+        String requestJson = objectMapper.writeValueAsString(taskDTO);
+
+        mockMvc.perform(post("/tasks")
+                .with(httpBasic("moana", "22433093007"))
+                .contentType(APPLICATION_JSON)
+                .content(requestJson))
+                .andExpect(status().isOk());
+
+        Mockito.verify(repositoryTask, Mockito.times(1)).save(task);
+
+        EntityTask newTask = repositoryTask.findByIdAndActiveTrue(task.getId());
+
+        Assertions.assertEquals(task, newTask);
+    }
 }

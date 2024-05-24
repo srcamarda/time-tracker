e546aa9d938581289ed1efa744de4cdfe648b091
Thiago Camarda <thiago.camarda@gmail.com>
2024-05-24 19:04:16 -0300

    Add new mocks

--- backend/src/test/java/com/dev/timetracker/controller/ControllerMocks.java
+++ backend/src/test/java/com/dev/timetracker/controller/ControllerMocks.java
@@ -1,30 +1,40 @@
 package com.dev.timetracker.controller;
 
+import com.dev.timetracker.dto.project.DTOCreateProject;
 import com.dev.timetracker.dto.task.DTOCreateTask;
 import com.dev.timetracker.dto.user.DTOCreateUser;
 import com.dev.timetracker.dto.user.DTOLoginUser;
+import com.dev.timetracker.entity.EntityProject;
 import com.dev.timetracker.entity.EntityTask;
 import com.dev.timetracker.entity.EntityUser;
 import com.dev.timetracker.utility.category.Role;
 import com.dev.timetracker.utility.category.Tag;
-import org.junit.jupiter.api.BeforeAll;
 import org.mockito.Mockito;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.Set;
 
 public class ControllerMocks {
 
     static DTOCreateUser userDTO;
     static DTOCreateUser userDTO2;
     static DTOLoginUser loginDTO;
-    static DTOLoginUser loginDTO2;
     static EntityUser user;
     static EntityUser user2;
     static EntityUser userMock;
 
     static DTOCreateTask taskDTO;
+    static DTOCreateTask taskDTO2;
     static EntityTask task;
+    static EntityTask task2;
+    static EntityTask taskMock;
+
+    static DTOCreateProject projectDTO;
+    static DTOCreateProject projectDTO2;
+    static EntityProject project;
+    static EntityProject project2;
+    static EntityProject projectMock;
 
     public static void userMocks() {
 
@@ -63,11 +73,6 @@ public class ControllerMocks {
                 userDTO.cpf()
         );
 
-        loginDTO2 = new DTOLoginUser(
-                userDTO2.username(),
-                userDTO2.cpf()
-        );
-
         user = new EntityUser(userDTO);
         user2 = new EntityUser(userDTO2);
 
@@ -80,20 +85,77 @@ public class ControllerMocks {
         userMock.setCpf(user.getCpf());
     }
 
-    @BeforeAll
     public static void taskMocks() {
 
         taskDTO = new DTOCreateTask(
                 1L,
-                "O Teste do Teste",
-                "vamos testar em tudo",
-                Tag.IN_PROGRESS,
+                "Creditos",
+                "Corrigir creditos finais",
+                Tag.URGENT,
+                user,
+                Timestamp.valueOf(LocalDateTime.now()),
+                null
+        );
+
+        taskDTO2 = new DTOCreateTask(
+                2L,
+                "Legenda",
+                "Finalizar legendas em PT",
+                Tag.IMPORTANT,
                 user,
                 Timestamp.valueOf(LocalDateTime.now()),
                 null
         );
 
         task = new EntityTask(taskDTO);
+        task2 = new EntityTask(taskDTO2);
+
         task.setId(1L);
+        task2.setId(2L);
+
+        taskMock = Mockito.mock(EntityTask.class);
+        taskMock.setId(task.getId());
+        taskMock.setTitle(task.getTitle());
+        taskMock.setDescription(task.getDescription());
+    }
+
+    public static void projectMocks() {
+
+        Set<EntityUser> setUsers = Set.of(user, user2);
+        Set<EntityTask> setTasks = Set.of(task, task2);
+        Set<Tag> setTags = Set.of(Tag.IN_PROGRESS, Tag.IMPORTANT);
+
+        projectDTO = new DTOCreateProject(
+                1L,
+                "Moana",
+                "Moana o filme",
+                Timestamp.valueOf(LocalDateTime.now()),
+                null,
+                setUsers,
+                setTasks,
+                setTags
+        );
+
+        projectDTO2 = new DTOCreateProject(
+                2L,
+                "Moana 2",
+                "O inimigo agora e outro",
+                Timestamp.valueOf(LocalDateTime.now()),
+                null,
+                setUsers,
+                setTasks,
+                setTags
+        );
+
+        project = new EntityProject(projectDTO);
+        project2 = new EntityProject(projectDTO2);
+
+        project.setId(1L);
+        project2.setId(2L);
+
+        projectMock = Mockito.mock(EntityProject.class);
+        projectMock.setId(project.getId());
+        projectMock.setTitle(project.getTitle());
+        projectMock.setDescription(project.getDescription());
     }
 }

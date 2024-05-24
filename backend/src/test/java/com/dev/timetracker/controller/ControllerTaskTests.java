1e52f890895c8182a621c7a207df663ea2fb967d
Thiago Camarda <thiago.camarda@gmail.com>
2024-05-23 21:14:13 -0300

    Add test classes for ControllerTask and ControllerProject

---
+++ backend/src/test/java/com/dev/timetracker/controller/ControllerTaskTests.java
@@ -0,0 +1,8 @@
+package com.dev.timetracker.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ControllerTaskTests {
+}

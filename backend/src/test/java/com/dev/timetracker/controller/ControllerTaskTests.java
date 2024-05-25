22bbc443679fdcedc8edfdef4ffff99c783dbb1a
Pedro Antonio Souza <pedroantoniiodm@gmail.com>
2024-05-24 22:33:39 -0300

    Add New Tests ControllerTaskTests

--- backend/src/test/java/com/dev/timetracker/controller/ControllerTaskTests.java
+++ backend/src/test/java/com/dev/timetracker/controller/ControllerTaskTests.java
@@ -1,6 +1,8 @@
 package com.dev.timetracker.controller;
 
+import com.dev.timetracker.dto.task.DTOUpdateTask;
 import com.dev.timetracker.entity.EntityTask;
+import com.dev.timetracker.repository.RepositoryProject;
 import com.dev.timetracker.repository.RepositoryTask;
 import com.dev.timetracker.repository.RepositoryUser;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -15,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static com.dev.timetracker.controller.ControllerMocks.*;
@@ -24,8 +27,7 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -44,6 +46,9 @@ public class ControllerTaskTests {
     @MockBean
     private RepositoryTask repositoryTask;
 
+    @MockBean
+    private RepositoryProject repositoryProject;
+
     @InjectMocks
     private ControllerTask controllerTask;
 
@@ -51,6 +56,7 @@ public class ControllerTaskTests {
     public static void initializeTestMocks() {
         userMocks();
         taskMocks();
+        projectMocks();
     }
 
     @Test
@@ -73,16 +79,67 @@ public class ControllerTaskTests {
     @Test
     public void shouldUpdateTask() throws Exception {
 
-        Mockito.when(repositoryTask.findByIdAndActiveTrue(task.getId())).thenReturn(task);
+        DTOUpdateTask updateTask = new DTOUpdateTask(
+                task.getId(),
+                task.getTitle(),
+                task.getDescription(),
+                task.getTag(),
+                task.getIdUser(),
+                task.getStartTime(),
+                task.getEndTime()
+        );
 
-        String requestJson = objectMapper.writeValueAsString(taskDTO);
+        Mockito.when(repositoryTask.getReferenceById(task.getId())).thenReturn(taskMock);
 
-        task.setEndTime(Timestamp.valueOf(LocalDateTime.now()));
+        String requestJson = objectMapper.writeValueAsString(updateTask);
 
         mockMvc.perform(put("/tasks")
                         .with(httpBasic(basicUser.username(), basicUser.cpf()))
                         .contentType(APPLICATION_JSON)
                         .content(requestJson))
                 .andExpect(status().isOk());
+
+//        Mockito.verify(taskMock, Mockito.times(1)).update(updateTask);
+    }
+
+    @Test
+    public void shouldActivateTask() throws Exception {
+
+        Mockito.when(repositoryTask.getReferenceById(taskMock.getId())).thenReturn(taskMock);
+
+        mockMvc.perform(put("/tasks/activate/" + taskMock.getId())
+                        .with(httpBasic(basicUser.username(), basicUser.cpf())))
+                .andExpect(status().isOk());
+
+        Mockito.verify(taskMock, Mockito.times(1)).activate();
+    }
+
+    @Test
+    public void shouldInactivateTask() throws Exception {
+
+        Mockito.when(repositoryTask.getReferenceById(taskMock.getId())).thenReturn(taskMock);
+
+        //When user is inactivated, it should return ok
+        mockMvc.perform(put("/tasks/inactivate/" + taskMock.getId())
+                        .with(SecurityMockMvcRequestPostProcessors.httpBasic(basicUser.username(), basicUser.cpf())))
+                .andExpect(status().isOk());
+
+        //Inactivate user must be called 1 time
+        Mockito.verify(taskMock, Mockito.times(1)).inactivate();
+    }
+
+    @Test
+    public void shouldDeleteTask() throws Exception {
+
+        Mockito.when(repositoryTask.getReferenceById(task.getId())).thenReturn(taskMock);
+        Mockito.when(repositoryProject.findByTasksId(task.getId())).thenReturn(List.of(projectMock));
+
+        mockMvc.perform(delete("/tasks/" + task.getId())
+                        .with(SecurityMockMvcRequestPostProcessors.httpBasic(basicUser.username(), basicUser.cpf())))
+                .andExpect(status().isOk());
+
+
+        Mockito.verify(repositoryTask, Mockito.times(1)).deleteById(task.getId());
+        Mockito.verify(repositoryProject, Mockito.times(1)).save(Mockito.any());
     }
 }

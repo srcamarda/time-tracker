80c83e3b07a17cf5fac1540c03ee4f5ca39c802c
Thiago Camarda <thiago.camarda@gmail.com>
2024-05-25 23:17:00 -0300

    Add test for main

--- backend/src/test/java/com/dev/timetracker/TimeTrackerApplicationTests.java
+++ backend/src/test/java/com/dev/timetracker/TimeTrackerApplicationTests.java
@@ -6,8 +6,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TimeTrackerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
+    @Test
+    void timeTrackerMainTest() {
+        TimeTrackerApplication.main(new String[]{});
+    }
 }

555233be1d0711e191aac94a93e951345fc4e0ff
Pedro Antonio Souza <pedroantoniiodm@gmail.com>
2024-05-25 22:37:35 -0300

    Update ControllerProjectsTest

--- backend/src/test/java/com/dev/timetracker/controller/ControllerProjectTests.java
+++ backend/src/test/java/com/dev/timetracker/controller/ControllerProjectTests.java
@@ -1,6 +1,11 @@
 package com.dev.timetracker.controller;
 
+import com.dev.timetracker.dto.project.DTOListProject;
+import com.dev.timetracker.dto.project.DTOUpdateProject;
+import com.dev.timetracker.dto.task.DTOListTask;
+import com.dev.timetracker.dto.task.DTOUpdateTask;
 import com.dev.timetracker.entity.EntityProject;
+import com.dev.timetracker.entity.EntityTask;
 import com.dev.timetracker.repository.RepositoryProject;
 import com.dev.timetracker.repository.RepositoryTask;
 import com.dev.timetracker.repository.RepositoryUser;
@@ -16,19 +21,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.dev.timetracker.mocks.ProjectMocks.*;
-import static com.dev.timetracker.mocks.TaskMocks.taskMock;
-import static com.dev.timetracker.mocks.TaskMocks.taskMocks;
+import static com.dev.timetracker.mocks.TaskMocks.*;
+import static com.dev.timetracker.mocks.TaskMocks.task;
+import static com.dev.timetracker.mocks.UserMocks.user;
 import static com.dev.timetracker.mocks.UserMocks.userMocks;
 import static com.dev.timetracker.security.SecurityConfigTest.basicUser;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -77,6 +89,140 @@ public class ControllerProjectTests {
         EntityProject newProject = repositoryProject.findByIdAndActiveTrue(project.getId());
         Assertions.assertEquals(project, newProject);
     }
+
+    @Test
+    public void shouldReturnProject() throws Exception {
+
+        Mockito.when(repositoryProject.findByIdAndActiveTrue(project.getId())).thenReturn(project);
+
+        String responseJson = objectMapper.writeValueAsString(new DTOListProject(project));
+
+        mockMvc.perform(get("/projects/" + project.getId())
+                        .with(httpBasic(basicUser.username(), basicUser.cpf())))
+                .andExpect(status().isOk())
+                .andExpect(content().json(responseJson));
+
+    }
+
+    @Test
+    public void shouldReturnProjectByUserId() throws Exception {
+
+        List<EntityProject> projects = Arrays.asList(project, project2);
+
+        Mockito.when(repositoryProject.findByUsersId(user.getId())).thenReturn(projects);
+
+        String responseJson = objectMapper.writeValueAsString(projects.stream().map(DTOListProject::new).collect(Collectors.toList()));
+
+        mockMvc.perform(get("/projects/users/" + user.getId()).
+                        with(httpBasic(basicUser.username(), basicUser.cpf())))
+                .andExpect(status().isOk())
+                .andExpect(content().json(responseJson));
+
+    }
+
+    @Test
+    public void shouldlistReturnProject() throws Exception {
+
+        Page<EntityProject> projects = new PageImpl<>(List.of(project, project2));
+
+        Mockito.when(repositoryProject.findAllByActiveTrue(Mockito.any(Pageable.class))).thenReturn(projects);
+
+        String responseJson = objectMapper.writeValueAsString(projects.map(DTOListProject::new).getContent());
+
+        mockMvc.perform(get("/projects").
+                        with(httpBasic(basicUser.username(), basicUser.cpf())))
+                .andExpect(status().isOk())
+                .andExpect(content().json(responseJson));
+    }
+
+    @Test
+    public void shouldlistAllReturnProjects() throws Exception {
+
+        Page<EntityProject> projects = new PageImpl<>(List.of(project, project2));
+
+        Mockito.when(repositoryProject.findAllByActiveTrue(Mockito.any(Pageable.class))).thenReturn(projects);
+
+        String responseJson = objectMapper.writeValueAsString(projects.map(DTOListProject::new).getContent());
+
+        mockMvc.perform(get("/projects/all").
+                        with(httpBasic(basicUser.username(), basicUser.cpf())))
+                .andExpect(status().isOk())
+                .andExpect(content().json(responseJson));
+    }
+
+    @Test
+    public void shouldReturnListUsersInProject() throws Exception {
+
+        Mockito.when(repositoryProject.getReferenceById(project.getId())).thenReturn(projectMock);
+
+        mockMvc.perform(get("/projects/" + project.getId() +"/users").
+                        with(httpBasic(basicUser.username(), basicUser.cpf())))
+                .andExpect(status().isOk());
+    }
+
+//    @Test
+//    public void shouldReturnListTasksInProject() throws Exception {
+//
+//        List<EntityTask> tasks = Arrays.asList(task, task2);
+//
+//        Mockito.when(repositoryTask.findByProjectIdAndActiveTrue(project.getId())).thenReturn(tasks);
+//
+//        String responseJson = new ObjectMapper().writeValueAsString(tasks.stream().map(DTOListTask::new).collect(Collectors.toList()));
+//
+//
+//        mockMvc.perform(get("/projects/" + project.getId() +"/tasks")
+//                        .with(httpBasic(basicUser.username(), basicUser.cpf()))
+//                        .contentType(APPLICATION_JSON))
+//                .andExpect(status().isOk())
+//                .andExpect(content().json(responseJson));
+//    }
+
+    @Test
+    public void shouldReturnListTasksNoContentInProject() throws Exception {
+
+        Mockito.when(repositoryTask.findByProjectIdAndActiveTrue(project.getId())).thenReturn(Arrays.asList());
+
+        mockMvc.perform(get("/projects/"+ project.getId() +"/tasks")
+                        .with(httpBasic(basicUser.username(), basicUser.cpf()))
+                        .contentType(APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
+    public void shouldUpdateProject() throws Exception {
+
+        DTOUpdateProject updateProject = new DTOUpdateProject(
+                project.getId(),
+                project.getTitle(),
+                project.getDescription(),
+                project.getStartTime(),
+                project.getStartTime()
+        );
+
+        Mockito.when(repositoryProject.getReferenceById(project.getId())).thenReturn(projectMock);
+
+        String requestJson = objectMapper.writeValueAsString(updateProject);
+
+        mockMvc.perform(put("/projects")
+                        .with(httpBasic(basicUser.username(), basicUser.cpf()))
+                        .contentType(APPLICATION_JSON)
+                        .content(requestJson))
+                .andExpect(status().isOk());
+
+//        Mockito.verify(projectMock, Mockito.times(1)).update(updateProject);
+    }
+
+//    @Test
+//    public void shouldAddUserToProject() throws Exception {
+//
+//        Mockito.when(repositoryProject.getReferenceById(project.getId())).thenReturn(project);
+//        Mockito.when(repositoryUser.getReferenceById(user.getId())).thenReturn(user);
+//
+//        mockMvc.perform(put("/projects/" + project.getId() + "/users/" + user.getId())
+//                        .with(httpBasic(basicUser.username(), basicUser.cpf()))
+//                        .contentType(APPLICATION_JSON))
+//                .andExpect(status().isOk());
+//    }
 
     @Test
     public void shouldActivateProject() throws Exception {
